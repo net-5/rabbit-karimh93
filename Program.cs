2a063a14083ee254d28a4439e73b38033809eb80
karimh93 <karim.husein0293@gmail.com>
2019-06-28 15:25:43 +0300

    Rabbit Homework

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rabbit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rabbit rabbit = new Rabbit(Eyes.Blue, Fur.Brown, Gender.Male, new DateTime(1993 ,06, 02));
+            Console.WriteLine(rabbit.BirthDate);
+            
+        }
+    }
+}
---
+++ Rabbit.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rabbit
+{
+
+    public enum Eyes
+    {
+        Blue,
+        Red,
+        Black
+    }
+
+    public enum Fur
+    {
+        White,
+        Brown,
+        Black,
+        Grey
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    class Rabbit
+    {
+        public DateTime BirthDate { get; set; }
+        public Eyes Eyes { get; }
+        public Gender Gender { get; }
+        public Fur Fur { get; }
+
+        public DateTime Today { get; }
+
+      
+       
+        
+
+        public Rabbit(Eyes eyes,Fur fur,Gender gender,DateTime birthDate)
+        {
+            Eyes = eyes;
+            Fur = fur;
+            Gender = gender;
+            BirthDate = birthDate;
+           
+            double diff = (int)((DateTime.Now- birthDate).TotalDays)/362.25;
+           
+
+            Console.WriteLine($"The rabbit has {Eyes} eyes,{Fur} fur,is a {Gender} and has {Math.Round(diff)} years old ");
+
+        }
+
+   
+        public static void Moves()
+        {
+            Console.WriteLine("The rabbit has been moved");
+        }
+        public static void Sleeps()
+        {
+            Console.WriteLine("The rabbit has been sleeping");
+        }
+        public static void Eats()
+        {
+            Console.WriteLine("The rabbit has been eating");
+        }
+    }
+}
